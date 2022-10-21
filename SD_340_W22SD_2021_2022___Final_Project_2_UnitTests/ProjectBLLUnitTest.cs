6bde5256cfb4cd4b8be3873626eb309649f5ec0a
BinitBudwal <binitsainibudwal@student.mitt.ca>
2022-10-21 16:57:28 -0500

    ProjectBLLUnitTest Initial

--- SD_340_W22SD_2021_2022___Final_Project_2_UnitTests/ProjectBLLUnitTest.cs
+++ SD_340_W22SD_2021_2022___Final_Project_2_UnitTests/ProjectBLLUnitTest.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using SD_340_W22SD_2021_2022___Final_Project_2.BLL;
+using SD_340_W22SD_2021_2022___Final_Project_2.Models;
 
 namespace SD_340_W22SD_2021_2022___Final_Project_2_UnitTests
 {
     [TestClass]
     public class ProjectBLLUnitTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ProjectBusinessLogic ProjectBusinessLogic;
+        public readonly UserManager<ApplicationUser> UserManager;
+
+        public ProjectBLLUnitTest()
         {
+            
         }
     }
 }
